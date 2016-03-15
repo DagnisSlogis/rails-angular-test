6ec0ee68287eacab2c355dc8e369a3ca9f8a16a1
DagnisSlogis <dagnis.slogis@gmail.com>
2016-03-15 10:08:55 +0200

    Can add new posts

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,21 +1,31 @@
 angular.module('creativeNews', ['ui.router', 'templates'])
 
 .config([
-  '$stateProvider',
-  '$urlRouterProvider',
-  function($stateProvider, $urlRouterProvider) {
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
 
-    $stateProvider
-      .state('home', {
-        url: '/home',
-        templateUrl: 'home/_home.html',
-        controller: 'MainCtrl'
-      })
-      .state('posts', {
-        url: '/posts/{id}',
-        templateUrl: 'posts/_posts.html',
-        controller: 'PostsCtrl'
-      });
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: 'home/_home.html',
+      controller: 'MainCtrl',
+      resolve: {
+        postPromise: ['posts',
+          function(posts){
+            return posts.getAll();
+          }]
+		  }})
 
-    $urlRouterProvider.otherwise('home');
+		.state('posts', {
+		  url: '/posts/{id}',
+		  templateUrl: 'posts/_posts.html',
+		  controller: 'PostsCtrl',
+      resolve: {
+        post: ['$stateParams', 'posts', function($stateParams, posts) {
+          return posts.get($stateParams.id);
+        }]
+      }
+    });
+  $urlRouterProvider.otherwise('home');
 }]);
